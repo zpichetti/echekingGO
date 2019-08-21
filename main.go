ecdca8c36a60b5a4a34facf23bc370a5688b2aa7
Xavier PICHETTI <pichon.pich@free.fr>
2019-08-21 16:50:44 +0200

    add format for date and change select file

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
@@ -29,15 +32,30 @@ func getListID(file string, sheet string) (list []string) {
 }
 
 func main() {
-	sheet := "Feuil1"
-	f1 := getListID("./ancien.xlsx", sheet)
+	// list xlsx files
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		fmt.Println(err)
+	}
+	listFiles := []string{}
+	for _, f := range files {
+		if strings.HasSuffix(f.Name(), ".xlsx") {
+			listFiles = append(listFiles, f.Name())
+		}
+	}
+	fmt.Printf("Comparaison des fichiers %s avec le fichier %s \n", listFiles[0], listFiles[1])
+	fmt.Println("Patienter...")
 
-	f2, err := excelize.OpenFile("./nouveau.xlsx")
+	// List Old contract list
+	sheet := "Feuil1"
+	f1 := getListID(fmt.Sprintf("./%s", listFiles[0]), sheet)
+
+	// extract new contract
+	f2, err := excelize.OpenFile(fmt.Sprintf("./%s", listFiles[1]))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 	rowsf2, err := f2.GetRows(sheet)
 	list := [][]string{}
 	for _, row := range rowsf2 {
@@ -49,7 +67,7 @@ func main() {
 
 	// write new file with result
 	f := excelize.NewFile()
-	// Set value of a cell.
+	// Set first row value.
 	f.SetCellValue("Sheet1", "A1", "Code chantier")
 	f.SetCellValue("Sheet1", "B1", "Libellé du chantier")
 	f.SetCellValue("Sheet1", "C1", "N° Marché")
@@ -60,24 +78,48 @@ func main() {
 	f.SetCellValue("Sheet1", "H1", "Date envoi agrément")
 	f.SetCellValue("Sheet1", "I1", "Statut M.")
 
+	// Set value row of new contract
+
 	for i, row := range list {
+		i1, _ := strconv.ParseInt(row[6], 10, 64)
+		i2, _ := strconv.ParseInt(row[7], 10, 64)
+
 		f.SetCellValue("Sheet1", fmt.Sprintf("A%d", i+2), row[0])
 		f.SetCellValue("Sheet1", fmt.Sprintf("B%d", i+2), row[1])
 		f.SetCellValue("Sheet1", fmt.Sprintf("C%d", i+2), row[2])
 		f.SetCellValue("Sheet1", fmt.Sprintf("D%d", i+2), row[3])
 		f.SetCellValue("Sheet1", fmt.Sprintf("E%d", i+2), row[4])
 		f.SetCellValue("Sheet1", fmt.Sprintf("F%d", i+2), row[5])
-		f.SetCellValue("Sheet1", fmt.Sprintf("G%d", i+2), row[6])
-		f.SetCellValue("Sheet1", fmt.Sprintf("H%d", i+2), row[7])
+		if i1 == 0 {
+			f.SetCellValue("Sheet1", fmt.Sprintf("G%d", i+2), "")
+		} else {
+			f.SetCellValue("Sheet1", fmt.Sprintf("G%d", i+2), i1)
+		}
+		if i2 == 0 {
+			f.SetCellValue("Sheet1", fmt.Sprintf("H%d", i+2), "")
+		} else {
+			f.SetCellValue("Sheet1", fmt.Sprintf("H%d", i+2), i2)
+		}
 		f.SetCellValue("Sheet1", fmt.Sprintf("I%d", i+2), row[8])
 	}
+
+	// set time format
+	f.SetColWidth("Sheet1", "A", "H", 13)
+	f.SetColWidth("Sheet1", "D", "D", 26)
+	f.SetColWidth("Sheet1", "B", "B", 26)
+	f.SetColWidth("Sheet1", "F", "F", 26)
+	style, err := f.NewStyle(`{"number_format": 14}`)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = f.SetCellStyle("Sheet1", "G2", fmt.Sprintf("H%d", len(list)+1), style)
 	// Save xlsx file by the given path.
-	err2 := f.SaveAs("./nouveau_contrat.xlsx")
+	err2 := f.SaveAs("./comparaison.xlsx")
 	if err2 != nil {
 		fmt.Println(err2)
 	}
 	var sizeList int
 	fmt.Println("Done !!!")
-	fmt.Printf("Il y a %d nouveau(x) contrat(s)", len(list))
+	fmt.Printf("Il y a %d nouveaux marchés", len(list))
 	fmt.Scanln(&sizeList)
 }
