4c49ee6378a057d1f9bccae75c59d07d68ed7e23
Xavier PICHETTI <pichon.pich@free.fr>
2019-08-21 10:59:10 +0200

    init file

--- main.go
+++ main.go
@@ -7,20 +7,27 @@ import (
 )
 
 func main() {
-	xlsx, err := excelize.OpenFile("./Book1.xlsx")
+	f1, err := excelize.OpenFile("./doc1.xlsx")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	// Get value from cell by given worksheet name and axis.
-	cell := xlsx.GetCellValue("Sheet1", "B2")
-	fmt.Println(cell)
 	// Get all the rows in the Sheet1.
-	rows := xlsx.GetRows("Sheet1")
+	rows, err := f1.GetRows("Sheet1")
 	for _, row := range rows {
 		for _, colCell := range row {
 			fmt.Print(colCell, "\t")
 		}
 		fmt.Println()
+	}
+
+	// write new file with result
+	f := excelize.NewFile()
+	// Set value of a cell.
+	f.SetCellValue("Sheet1", "B2", 300)
+	// Save xlsx file by the given path.
+	err2 := f.SaveAs("./Result.xlsx")
+	if err2 != nil {
+		fmt.Println(err2)
 	}
 }
